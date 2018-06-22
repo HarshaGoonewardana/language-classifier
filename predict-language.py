5ac35c3cb9f053622f7cc1bdb53ee01c2e9d9386
Chris <messiercr@gmail.com>
2018-06-22 15:44:10 -0400

    included more utilities in predict

--- predict-language.py
+++ predict-language.py
@@ -1,4 +1,8 @@
+import os
+import glob
 import argparse
+import random
+import string
 
 import torch
 
@@ -12,7 +16,25 @@ rnn = torch.load('checkpoints/language-classifier.pth')
 DATA_FILES = 'data/languages/*.txt'
 
 
-def assemble_data(data_glob, n=10000):
+def find_files(path):
+    return glob.glob(path)
+
+
+def unicode_to_ascii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn' and c in string.ascii_letters
+    )
+
+
+def read_lines(filename):
+    with open(filename, encoding='utf-8') as file:
+        lines = file.read().strip().split('\n')
+
+    return lines
+
+
+def assemble_data(data_glob, n=50000):
     category_lines = {}
     all_categories = []
     for file in find_files(data_glob):
@@ -25,12 +47,24 @@ def assemble_data(data_glob, n=10000):
     return category_lines, all_categories
 
 
-def evaluate(line_tensor, model):
-    hidden = model.init_hidden()
-    for i in range(line_tensor.size()[0]):
-        output, hidden = model.forward(line_tensor[i], hidden)
+def category_from_output(output):
+    top_n, top_i = output.topk(1)
+    category_i = top_i[0].item()
 
-    return output
+    return category_i
+
+def letter_to_index(letter):
+    all_letters = string.ascii_letters
+
+    return all_letters.find(letter)
+
+
+def letter_to_tensor(letter):
+    n_letters = len(string.ascii_letters)
+    tensor = torch.zeros(1, n_letters)
+    tensor[0][letter_to_index(letter)] = 1
+
+    return tensor
 
 
 def line_to_tensor(line):
@@ -40,6 +74,31 @@ def line_to_tensor(line):
         tensor[li][0][letter_to_index(letter)] = 1
 
     return tensor
+
+
+def random_choice(l):
+    return l[random.randint(0, len(l) - 1)]
+
+
+def random_training_example(data):
+    all_categories = list(data.keys())
+    category = random_choice(all_categories)
+    line = random_choice(data[category])
+    category_tensor = torch.tensor(
+        [all_categories.index(category)],
+        dtype=torch.long
+    )
+    line_tensor = line_to_tensor(line)
+
+    return category, line, category_tensor, line_tensor
+
+
+def evaluate(line_tensor, model):
+    hidden = model.init_hidden()
+    for i in range(line_tensor.size()[0]):
+        output, hidden = model.forward(line_tensor[i], hidden)
+
+    return output
 
 
 def predict(line, n_predictions=3):
