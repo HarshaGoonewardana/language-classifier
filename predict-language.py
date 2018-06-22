cbb4a656e76e5f78c1f55cfff64c34e1dd4d4c30
Chris <messiercr@gmail.com>
2018-06-22 16:04:22 -0400

    moved back to importing to avoid confusion

--- predict-language.py
+++ predict-language.py
@@ -6,99 +6,16 @@ import string
 
 import torch
 
+from utils import assemble_data, evaluate, line_to_tensor
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('word', type=str)
-parser.add_argument('--n-predictions', type=int, required=False, default=3)
+parser.add_argument('-n', type=int, required=False, default=3)
 args = parser.parse_args()
 
 rnn = torch.load('checkpoints/language-classifier.pth')
 DATA_FILES = 'data/languages/*.txt'
-
-
-def find_files(path):
-    return glob.glob(path)
-
-
-def unicode_to_ascii(s):
-    return ''.join(
-        c for c in unicodedata.normalize('NFD', s)
-        if unicodedata.category(c) != 'Mn' and c in string.ascii_letters
-    )
-
-
-def read_lines(filename):
-    with open(filename, encoding='utf-8') as file:
-        lines = file.read().strip().split('\n')
-
-    return lines
-
-
-def assemble_data(data_glob, n=50000):
-    category_lines = {}
-    all_categories = []
-    for file in find_files(data_glob):
-        category = os.path.basename(file).split('.')[0]
-        all_categories.append(category)
-        lines = read_lines(file)
-        random.shuffle(lines)
-        category_lines[category] = lines[:n] if n < len(lines) else lines
-
-    return category_lines, all_categories
-
-
-def category_from_output(output):
-    top_n, top_i = output.topk(1)
-    category_i = top_i[0].item()
-
-    return category_i
-
-def letter_to_index(letter):
-    all_letters = string.ascii_letters
-
-    return all_letters.find(letter)
-
-
-def letter_to_tensor(letter):
-    n_letters = len(string.ascii_letters)
-    tensor = torch.zeros(1, n_letters)
-    tensor[0][letter_to_index(letter)] = 1
-
-    return tensor
-
-
-def line_to_tensor(line):
-    n_letters = len(string.ascii_letters)
-    tensor = torch.zeros(len(line), 1, n_letters)
-    for li, letter in enumerate(line):
-        tensor[li][0][letter_to_index(letter)] = 1
-
-    return tensor
-
-
-def random_choice(l):
-    return l[random.randint(0, len(l) - 1)]
-
-
-def random_training_example(data):
-    all_categories = list(data.keys())
-    category = random_choice(all_categories)
-    line = random_choice(data[category])
-    category_tensor = torch.tensor(
-        [all_categories.index(category)],
-        dtype=torch.long
-    )
-    line_tensor = line_to_tensor(line)
-
-    return category, line, category_tensor, line_tensor
-
-
-def evaluate(line_tensor, model):
-    hidden = model.init_hidden()
-    for i in range(line_tensor.size()[0]):
-        output, hidden = model.forward(line_tensor[i], hidden)
-
-    return output
 
 
 def predict(line, n_predictions=3):
@@ -118,4 +35,4 @@ def predict(line, n_predictions=3):
 
 
 if __name__ == "__main__":
-    predict(args.word, args.n_predictions)
+    predict(args.word, args.n)
